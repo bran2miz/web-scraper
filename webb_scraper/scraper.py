eb75005826cd189e9243ff4887959f73441b6a66
brandonmizutani <bran2miz@gmail.com>
2022-02-05 15:40:31 -0800

    finished lab 17 and all tests have passed

--- webb_scraper/scraper.py
+++ webb_scraper/scraper.py
@@ -1,10 +1,20 @@
 import requests
+from bs4 import BeautifulSoup
 
-URL = 'https://en.wikipedia.org/wiki/RuPaul%27s_Drag_Race'
+def get_passages(url):
+    page = requests.get(url)
+    matches = BeautifulSoup(page.content, "html.parser").find_all(title="Wikipedia:Citation needed")
+    return [match.find_parent('p').text for match in matches]
 
-page = requests.get(URL)
+def get_citations_needed_count(url):
+    passages = get_passages(url)
+    return len(passages)
+    
+def get_citations_needed_report(url):
+    passages = get_passages(url)
+    return '\n'.join([passage.strip() for passage in passages])
 
-print (page.content)
-
-if __name__ == "main":
-  pass
+if __name__ == "__main__":
+    url = "https://en.wikipedia.org/wiki/Cnut#Statesmanship"
+    print(get_citations_needed_count(url))
+    print(get_citations_needed_report(url))
